4438d0f703ad3da2abfc4fec514a9a170fe3af6c
AnatoliiBublyk <anatoliibublyk2004@gmail.com>
2023-10-09 01:15:26 +0300

    feat: add connection to database

---
+++ src/UserSubscriptionManagement.Infrastructure/Database/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace UserSubscriptionManagement.Infrastructure.Database;
+
+public class DbContext
+{
+    private readonly string _connectionString;
+    public DbContext(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+    public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+}
