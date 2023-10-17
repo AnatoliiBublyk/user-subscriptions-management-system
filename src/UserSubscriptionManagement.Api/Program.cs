d661ab1d32f54f67fc258f17305e98be53f0deec
AnatoliiBublyk <anatoliibublyk2004@gmail.com>
2023-10-17 14:23:36 +0300

    feat: add registration of repositories

--- src/UserSubscriptionManagement.Api/Program.cs
+++ src/UserSubscriptionManagement.Api/Program.cs
@@ -1,5 +1,6 @@
 using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
+using UserSubscriptionManagement.Application.Repositories;
 using UserSubscriptionManagement.Infrastructure.Database;
 using UserSubscriptionManagement.Infrastructure.Repository;
 
@@ -11,6 +12,10 @@ builder.Services.AddDbContext<UserSubscriptionsManagementContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         sqlOptions => sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
+
+builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserSubscriptionRepository, UserSubscriptionRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
